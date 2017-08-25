ccc740c9cab0abfb1bc4b0877d153c0e6c2841a3
Gunhee Jo <ckhee93@gmail.com>
2017-08-25 10:46:53 +0900

    upload file

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+
+app.get('/',function(req,res){
+    console.log(req["x"]);
+    console.log(req["y"]);
+    res.send("222");
+});
+
+app.listen(8888,function(){
+    console.log("sdf");
+});
+
