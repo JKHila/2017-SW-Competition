70fe122ba04a3a635d708bf7cfde0dcfec40c1de
Gunhee Jo <ckhee93@gmail.com>
2017-08-29 16:26:17 +0900

    facebook login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,8 +5,11 @@ var index = express.Router();
 index.post('/signup',function(req,res){
     userController.sign_up(req,res);
 });
-index.get('/login',function(req,res){
+index.post('/login',function(req,res){
     userController.login(req,res);
+});
+index.post('/login/facebook',function(req,res,next){
+    userController.facebook_login(req,res,next);
 });
 
 module.exports = index;
